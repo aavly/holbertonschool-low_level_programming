3a94755564a245a0782a245b1472a2f46dcf869c
anna <annavyly@gmail.com>
2024-10-25 13:42:59 +0000

    3. alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabet in lowercase and then in uppercase
+ *		followed by a new line
+ *
+ * Return: always 0.
+ */
+
+int main(void)
+{
+	int lower = 97;
+	int upper = 65;
+
+	while (lower < 123)
+	{
+		putchar(lower);
+	}
+	while (upper < 91)
+	{
+		putchar(upper);
+	}
+	printf("\n");
+
+	return (0);
+}
