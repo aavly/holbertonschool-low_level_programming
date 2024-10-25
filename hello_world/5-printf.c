4bfbddd5c82f8ba52c3298ed149e7b0c35092d07
anna <annavyly@gmail.com>
2024-10-25 08:59:09 +0000

    adding a new line

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,8 +1,22 @@
+/*
+ * File: 5-printf.c
+ * Author: Anna Ly
+ *
+ */
+
+
 #include <stdio.h>
+
+/*
+ * Main - Prints "with proper grammar, but the outcome is a piece of art"
+ *		followed by a new line
+ *
+ * Return: always 0
+ */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
