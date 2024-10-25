d59a1a5c591d0126aded60a7f71f45515be394f2
anna <annavyly@gmail.com>
2024-10-25 09:40:34 +0000

    betty documentation fixup

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Printing exactly word for word without using printf() function.
+ *
+ * Return: always 0.
+ */
+
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
