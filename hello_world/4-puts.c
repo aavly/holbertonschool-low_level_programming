5fd6e1e33fd841309c991ff49a9f9304103de614
anna <annavyly@gmail.com>
2024-10-25 08:45:04 +0000

    fixing trailing whitespace

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,8 @@
-/*
- * File: 4.puts.c
- * Author: Anna ly - 9851@holbertonstudents.com
- */
-
 #include <stdio.h>
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	
+	puts("\"Programming is like building a multilingual puzzle\n");
+
 	return (0);
 }
