c2e058659cc226b26170b9f3a0f635645b0aca05
anna <annavyly@gmail.com>
2024-10-25 08:39:41 +0000

    gotta make betty happy

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,10 @@
+/*
+ * File: 4.puts.c
+ * Author: Anna ly - 9851@holbertonstudents.com
+ */
+
 #include <stdio.h>
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
