f75489ce66454baa58cc0f63261ea6be86a8bf1f
anna <annavyly@gmail.com>
2024-10-26 14:41:54 +0000

    2 - 10 x alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10x in lowercase
+ *			followed by a new line.
+ */
+
+void print_alphabet_x10(void)
+{
+	int counter = 1;
+	int ascii = 97;
+
+	while (counter < 11)
+	{
+		while (ascii < 123)
+		{
+			_putchar(ascii);
+			ascii++;
+		}
+		counter++;
+	}
+}
