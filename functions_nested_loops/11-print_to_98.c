91d3d4fade454dcbde5183bf2ade042e6c36f66b
anna <annavyly@gmail.com>
2024-10-26 16:35:48 +0000

    11 - 98 Battery Street, the OG

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,12 +8,24 @@
 
 void print_to_98(int n)
 {
-	while (n <= 98)
+	if (n < 98)
 	{
-		if (n != 98)
+		while (n < 98)
+		{
 			printf("%d, ", n);
-		else
-			printf("%d", n);
-		n++;
+			n++;
+		}
+		printf("98\n");
 	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+		printf("98\n");
+	}
+	else
+	printf("98\n");
 }
