4004180555446f3858bf2673ed1d65128da16177
anna <annavyly@gmail.com>
2024-10-26 13:58:57 +0000

    figuring out the header and helper files

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
-# define MAIN_H
+#define MAIN_H
 
-void _putchar();
+/*
+ * Description: Header file containing declarations
+ *		for all functions used in this directory
+ */
+
+int _putchar(char c);
 
 #endif
