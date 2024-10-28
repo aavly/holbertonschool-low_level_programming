8d3709f37ad830240a0afe2d8257f356f8eb6c9d
root <annavyly@gmail.com>
2024-10-28 07:58:10 +0000

    Betty hates me

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper() - Checks for uppercase character.
+ * _isupper - Checks for uppercase character.
  * @c: character to check case
  *
  * Return: 1 if c is uppercase, 0 otherwise.
