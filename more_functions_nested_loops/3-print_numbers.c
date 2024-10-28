0e2865a5ac23968c13d9c82bbee89ea0dcbc8739
root <annavyly@gmail.com>
2024-10-28 08:13:20 +0000

    3. The numbers speak for themselves

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0-9,
+ *			followed by a new line.
+ *
+ */
+
+void print_numbers(void)
+{
+	int counter = 48;
+
+	while (counter <= 57)
+	{
+		_putchar(counter);
+
+		counter++;
+	}
+}
