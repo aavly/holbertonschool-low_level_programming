cb82e4abb876fe313f478fefda84274054461858
anna <annavyly@gmail.com>
2024-10-28 15:46:41 +0000

    11. Advance question - Project 1

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints largest prime factor of 612852475143
+ *	- followed by a newline.
+ *
+ * Return: 0 on success.
+ */
+
+int main(void)
+{
+	long num = 612852475143;
+	long factor = 2;
+	long biggest = 0;
+
+	while (num % 2 == 0)
+	{
+		biggest = factor;
+		num /= 2;
+	}
+
+	for (factor = 3; factor * factor <= num; factor += 2)
+	{
+		while (num % factor == 0)
+		{
+			biggest = factor;
+			num /= factor;
+		}
+	}
+
+	if (num > 2)
+		biggest = num;
+
+	printf("%ld", biggest);
+	return (0);
+}
