f20e7ff8c633f509c3f0ba960ebfe656a53e2adf
anna <annavyly@gmail.com>
2024-10-28 15:59:06 +0000

    12: Advanded task - Project 1

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ * @n: an integer
+ *
+ *	not allowed to use the following:
+ *	- long
+ *	- arrays or pointers
+ *	- hard coded special values
+ */
+
+void print_numnber(int n);
+{
+	_putchar((n % 10) + 48);
+}
