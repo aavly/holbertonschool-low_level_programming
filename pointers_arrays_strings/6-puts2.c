b1ae1d7f1060be2819f64abb9a075e1c308c5905
anna <annavyly@gmail.com>
2024-10-29 13:07:59 +0000

    skeleton of Q6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character,
+ *		starting with the first character,
+ *		followed by a newline
+ *
+ * @s: string to be printed.
+ */
+
+void put2(char *str)
+{
+}
