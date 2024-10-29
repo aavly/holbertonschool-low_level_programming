640abe1fce1b7332056b2485d7b7851a22fbbe87
anna <annavyly@gmail.com>
2024-10-29 06:41:22 +0000

    gotta figure out the code soon

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: string to be measured.
+ *
+ * Return: (int) - length of a string.
+ */
+
+int _strlen(char *s)
+{
+
+}
