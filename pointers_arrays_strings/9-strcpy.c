cea46ac880551bd43bb79ef9e39cdeeac73961ad
anna <annavyly@gmail.com>
2024-10-30 02:14:21 +0000

    9. strcpy (first attempt)

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string pointed to by src,
+ *		including the terminating null byte '\0'
+ *		to the buffer pointed set to by dest.
+ *
+ * Return: the pointer to dest.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0, loop = 0;
+
+	while (*src != '\0')
+	{
+		i++;
+	}
+	i++;
+
+	while (loop < i)
+	{
+		_putchar(dest[i]);
+		loop++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
