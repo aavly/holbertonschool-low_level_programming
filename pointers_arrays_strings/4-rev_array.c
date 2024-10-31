1d643ef246b5f0d42bc7d96001ac658504254cb4
anna <annavyly@gmail.com>
2024-10-31 05:49:29 +0000

    skeleton for files 4&5

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses content of an array of ints.
+ * @a: array to be reversed.
+ * @n: number of elements in array to be reversed.
+ */
+
+void reverse_array(int *a, int n)
+{
+}
---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * string_toupper - converts all lowercase to upper.
+ * @ : array of char (string) to be checked.
+ *
+ * Return: converted string.
+ */
+
+char *string_toupper(char *)
+{
+}
