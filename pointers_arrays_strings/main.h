089336f3ab42fd676a3ec8f7519ebfbec220b36a
anna <annavyly@gmail.com>
2024-10-31 02:35:30 +0000

    updating main.h file with all functions used for this project

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -6,6 +6,9 @@
  *		for all functions used in this directory
  */
 
+/*
+ * project 2 functions
+ */
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -18,6 +21,18 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
+
+/*
+ * project 3 functions
+ */
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
 
 
 #endif
