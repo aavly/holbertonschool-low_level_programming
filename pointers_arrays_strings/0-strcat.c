d4a7ca52ab414f31c468a803ee7db41790bd16b6
anna <annavyly@gmail.com>
2024-10-31 02:53:45 +0000

    swapping out *src for src[s]

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -15,10 +15,10 @@ char *_strcat(char *dest, char *src)
 	int d = 0;
 	int s = 0;
 
-	while (*dest != '\0')
+	while (dest[d] != '\0')
 		d++;
 
-	while (*src != '\0')
+	while (src[s] != '\0')
 	{
 		dest[d] = src[s];
 		d++;
