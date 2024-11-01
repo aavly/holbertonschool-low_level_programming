b6167eec3e4f4dbf3683c54d52be04f930a53b5b
anna <annavyly@gmail.com>
2024-11-01 09:55:31 +0000

    4. I am a kind of paranoid in reverse. I suspect people of plotting to make me happy

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -3,9 +3,22 @@
 /**
  * reverse_array - reverses content of an array of ints.
  * @a: array to be reversed.
- * @n: number of elements in array to be reversed.
+ * @n: number of elements in array.
  */
 
 void reverse_array(int *a, int n)
 {
+	int placeholder;
+	int start = 0;
+	int end = n - 1;
+
+	while (start < end)
+	{
+		placeholder = a[start];
+		a[start] = a[end];
+		a[end] = placeholder;
+
+		start++;
+		end--;
+	}
 }
