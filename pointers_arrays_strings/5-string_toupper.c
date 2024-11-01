3d777e9bd23b5e4241b7ac69ec70a28ecd7ec92f
anna <annavyly@gmail.com>
2024-11-01 10:01:41 +0000

    declaring variable in parameter

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -7,6 +7,16 @@
  * Return: converted string.
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+		i++;
+	}
+
+	return (str);
 }
