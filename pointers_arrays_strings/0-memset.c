b6c1faf12dc057bab6325f3510ae6ad9cdd57882
anna <annavyly@gmail.com>
2024-11-05 14:27:07 +0000

    fixing logic

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -14,12 +14,12 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
-	char *s[];
 
 	while (i < n)
 	{
-		*s[i] = b;
+		s[i] = b;
+		i++;
 	}
 
-	return (*s);
+	return (s);
 }
