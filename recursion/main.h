aa6a8565100118f68b012798b083c82863c141f2
anna <annavyly@gmail.com>
2024-11-06 06:05:58 +0000

    setting up putchar and main.h files

---
+++ recursion/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Description: Header file containing declarations
+ *		for all functions used in this directory
+ */
+
+void _puts_recursion(char *s);
+
+#endif
