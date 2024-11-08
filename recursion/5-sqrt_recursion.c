f3ef346a0b6a198530b80674f841ddaafe101ab1
anna <annavyly@gmail.com>
2024-11-08 06:10:53 +0000

    adding a helper function

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -2,7 +2,6 @@
 
 /**
  * _sqrt_recursion - returns the natural square root of n.
- *
  * @n: number to check for square root.
  *
  * Return: (int) natural square root.
@@ -10,13 +9,17 @@
 
 int _sqrt_recursion(int n)
 {
-	int find = 0;
+	/**
+	 * helper - helper func to recursively calc sqrt
+	 */
+	int helper(int n, int subtractor, int count)
+	{
+		if (n < 0)
+			return (-1);
+		if (n == 0)
+			return (count);
 
-	find++;
-	find = n - _sqrt_recursion(n - 1);
-
-	if (n == 0)
-		return (find);
-	else
-		return (-1);
+		return (helper(n - subtractor, subtractor + 2, count + 1));
+	}
+	return (helper(n, 1, 0));
 }
