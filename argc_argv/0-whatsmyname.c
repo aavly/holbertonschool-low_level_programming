4be2cb97f98e0aedef3e6dfca203170f3504b057
anna <annavyly@gmail.com>
2024-11-08 08:43:59 +0000

    compilation error

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints program's name (argv[0]).
+ * @argc: arg count
+ * @argv: array of string pointers
+ *
+ * Return: always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	while (argc > 0)
+		printf("%s\n", argv[0]);
+
+	return (0);
+}
