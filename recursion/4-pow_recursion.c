818330cd653f850fa777b6db7e8854077555606e
anna <annavyly@gmail.com>
2024-11-08 05:31:15 +0000

    added a while loop

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -13,11 +13,13 @@
 int _pow_recursion(int x, int y)
 {
 	int power;
+	int counter = 0;
 
 	if (y < 0)
 		return (-1);
 
-	power = x * _pow_recursion(x, y - 1);
+	while (counter < y)
+		power = x * _pow_recursion(x, y - 1);
 
 	return (power);
 }
