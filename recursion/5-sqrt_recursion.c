2ae4c41daf4bcc895c16bd6841064e48c5a35d39
anna <annavyly@gmail.com>
2024-11-08 06:17:30 +0000

    splitting up functions

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * helper - helper func to recursively calc sqrt
+ * @n: number to find sqrt.
+ * @subtractor: subtracting -2 each loop
+ * @count: how many times we gotta loop.
+ *
+ * Return: sqrt or -1 if not vald.
+ */
+
+int helper(int n, int subtractor, int count)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (count);
+
+	return (helper(n - subtractor, subtractor + 2, count + 1));
+}
+
+/**
  * _sqrt_recursion - returns the natural square root of n.
  * @n: number to check for square root.
  *
@@ -9,17 +28,5 @@
 
 int _sqrt_recursion(int n)
 {
-	/**
-	 * helper - helper func to recursively calc sqrt
-	 */
-	int helper(int n, int subtractor, int count)
-	{
-		if (n < 0)
-			return (-1);
-		if (n == 0)
-			return (count);
-
-		return (helper(n - subtractor, subtractor + 2, count + 1));
-	}
 	return (helper(n, 1, 0));
 }
