fdd1650406c071eb96eeece2a801e7be31bd5636
anna <annavyly@gmail.com>
2024-11-08 08:52:14 +0000

    gcc attribute for unused argc

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -8,13 +8,8 @@
  * Return: always 0.
  */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int count = 0;
-	while (argc > 1 && count != 1)
-	{
-		printf("%s\n", argv[0]);
-		count++;
-	}
+	printf("%s\n", argv[0]);
 	return (0);
 }
