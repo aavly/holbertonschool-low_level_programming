acd4a4ab332ec0e917b87c695e78fe81b362075b
anna <annavyly@gmail.com>
2024-11-14 13:13:07 +0000

    0. Trust no one

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -3,15 +3,22 @@
 
 /**
  * malloc_checked - allocates memory using malloc.
- * 
+ *
  * @b: amount of bytes to allocate.
  *
  * Return: pointer to the allocated memory.
- *	if malloc fails, function should cause 
+ *	if malloc fails, function should cause
  *	normal process termination with a status
  *	value of 98.
  */
 
 void *malloc_checked(unsigned int b)
 {
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+
+	free(ptr);
 }
