aacfcb2c6259827c41bbfdee6b7a0c380ab66a58
anna <annavyly@gmail.com>
2024-11-15 06:18:22 +0000

    corrected betty documentation

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,15 +8,16 @@
  * @age: dog's age
  * @owner: dog's owner
  *
- * Description: Header file defining new struct, dog
+ * Description: Structure representing a dog:
+ *		it's name, age and owner's name.
  */
 
 typedef struct dog
 {
-	char *name;
-	float age;
-	char *owner;
-}dog_t;
+	char *name;	/** name of dog */
+	float age;	/** age of dog */
+	char *owner;	/** name of dog's owner */
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
