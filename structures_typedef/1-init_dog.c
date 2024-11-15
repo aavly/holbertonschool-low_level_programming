b2213b60eda1a0887f4fb4810982f9db01da7751
anna <annavyly@gmail.com>
2024-11-15 05:51:23 +0000

    added a fail safe

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stddef.h>
 
 /**
  * init_dog - function that initialises a variable
@@ -14,5 +15,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog dog1 = {name, age, owner};
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
