50b6faf804c35ad6347f515d110b56c1936fe87d
anna <annavyly@gmail.com>
2024-11-18 23:52:30 +0000

    checking

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -16,14 +16,18 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
+	i = 0;
 
 	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
 
-	for (i = 0; i < size; i++)
+	while (i < size)
 	{
 		if (cmp(array[i]) != 0)
+		{
 			return (i);
+		}
+		i++;
 	}
 	return (-1);
 }
