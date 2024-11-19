ceafc99eb4aee64de92c98423167ee4f7621f5c1
anna <annavyly@gmail.com>
2024-11-19 04:04:38 +0000

    finished writing all functions

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -21,3 +21,37 @@ int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
+/**
+ * op_mul - returns the product of two numbers.
+ * @a: first number
+ * @b: second number
+ * Return: product of a and b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns the quotient of two numbers.
+ * @a: first number
+ * @b: second number
+ * Return: quotient of a divided by b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns the remainder of the division
+ *	of two numbers.
+ * @a: first number
+ * @b: second number
+ * Return: remainder of the divison of a and b.
+ */
+int op_mod(int a,  int b)
+{
+	return (a % b);
+}
