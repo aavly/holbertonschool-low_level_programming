616ca66a7830351dac7a741d895b55003aa27258
anna <annavyly@gmail.com>
2024-11-19 03:59:47 +0000

    writing functions for op_functions.c

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * op_add - returns the sums of two numbers.
+ * @a: first number
+ * @b: second number
+ * Return: sum of a and b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns the difference of two numbers.
+ * @a: first number
+ * @b: second number
+ * Return: difference of a and b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
