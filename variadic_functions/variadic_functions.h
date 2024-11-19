a8ca839a40b5f7408daa76a841c6b86f568151d6
anna <annavyly@gmail.com>
2024-11-19 09:41:31 +0000

    setting up directory

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,15 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+/**
+ * Description: Header file containing declarations
+ *		for all functions used in this directory
+ */
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
