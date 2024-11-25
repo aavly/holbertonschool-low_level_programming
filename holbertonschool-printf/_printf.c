37039d01a39ccbb0a582e6973664239b1f7928ae
Radew-lab-pixel <ktmqliu@gmail.com>
2024-11-25 01:13:51 +0000

    Backup

---
+++ holbertonschool-printf/_printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _printf - function to process the argument based format
+ *
+ * @format : char array indicating the format of the argument
+ * Return: integer value of the total of char in argument
+ */
+
+int _printf(const char *format, ...)
+{
+
+}
