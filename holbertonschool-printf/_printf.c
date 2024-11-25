209e7ef62cfe45fb537e3ef41d59899ab5d71cba
Radew-lab-pixel <ktmqliu@gmail.com>
2024-11-25 02:17:56 +0000

    Backup

--- holbertonschool-printf/_printf.c
+++ holbertonschool-printf/_printf.c
@@ -12,10 +12,18 @@ int _printf(const char *format, ...)
 	int count; /* no. of characters to be return */
 	int loop_count; /* counter for while loop */
 	int length; /* length of string char *s */ 
+	char *s;
+	int i = 0;
 
 	va_list printList; /* declaring va variable printList*/
 	va_start(printList, format); /* intializing printList with last fixed argument being format */
 
+	s = va_arg(printList, char *);
 
+	while (format) && (format[i]) /* if format and format[i] are valid */
+	{
+		
+
+	}
 
 }
