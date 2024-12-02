e51c9ae1faecd3c9b5a42f0abfb64be4037f80c1
anna <annavyly@gmail.com>
2024-12-02 13:55:30 +0000

    writing data struct in header file

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -2,6 +2,22 @@
 #define LISTS_H
 
 /**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/**
  * Description: Header file containing declarations
  *		for all functions used in this directory
  */
