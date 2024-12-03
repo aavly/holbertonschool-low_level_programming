d6ec4873d8cc44d1429b3c3c1d1f315f13f78669
root <annavyly@gmail.com>
2024-12-03 08:41:43 +0000

    revising variable types and modifying traversing logic

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -9,17 +9,13 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	dlistint_t *traversingNode;
-	int nodeCount;
-
-	traversingNode = malloc(sizeof(dlistint_t));
-	if (traversingNode == NULL)
-		return (0);
+	const dlistint_t *traversingNode;
+	size_t nodeCount;
 
 	nodeCount = 0;
 	traversingNode = h->next;
 
-	while (h != NULL)
+	while (traversingNode != NULL)
 	{
 		printf("%d", traversingNode->n);
 		nodeCount = nodeCount + 1;
