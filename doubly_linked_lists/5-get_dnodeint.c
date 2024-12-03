df203ba45ea7492883a64ae9e26652eab0fea6dc
root <annavyly@gmail.com>
2024-12-03 13:40:50 +0000

    first attempt

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - gets node at index
+ * @head: head node of linked list
+ * @index: nth node to find
+ *
+ * Return: nth node of a linked list
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (i < index)
+	{
+		while (head != NULL)
+		{
+			head = head->next;
+			i = i + 1;
+		}
+		if (i != index - 1)
+			return (NULL);
+	}
+	return (head);
+}
