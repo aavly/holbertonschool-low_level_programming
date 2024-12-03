90a8d1fae06e500ac57ff8b6862b911c1f585de7
root <annavyly@gmail.com>
2024-12-03 09:07:46 +0000

    setup of task 2

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a new node at the beginning of linked list.
+ * @head: pointer to head node.
+ * @n: value associate with node
+ *
+ * Return: address of new element || NULL if failed.
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+}
