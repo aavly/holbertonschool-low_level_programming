3339649c64fb2c0ca6bf4836598478b11bb9ea0e
root <annavyly@gmail.com>
2024-12-03 08:54:41 +0000

    just setting it up, going for a walk now

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,26 +1,13 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all elements of list.
- * @h: head of list
+ * dlistint_len - returns number of elements in a list
+ * @h: head node of list.
  *
- * Return: number of nodes in list.
+ * Return: number of elements in list.
  */
 
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
-	const dlistint_t *traversingNode;
-	size_t nodeCount;
 
-	nodeCount = 0;
-	traversingNode = h->next;
-
-	while (traversingNode != NULL)
-	{
-		printf("%d", traversingNode->n);
-		nodeCount = nodeCount + 1;
-		traversingNode = traversingNode->next;
-	}
-
-	return (nodeCount);
 }
