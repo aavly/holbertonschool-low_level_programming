1fdc32f14315c43c9e098753ce528e044c4e7b02
anna <annavyly@gmail.com>
2024-12-06 00:45:45 +0000

    modified assignment and traversing logic

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -14,29 +14,49 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h,
 		unsigned int idx, int n)
 {
-	dlistint_t *newNode;
+	dlistint_t *newNode, *current;
 	int indexCount;
+
+	if (h == NULL)
+		return (NULL);
 
 	newNode = malloc(sizeof(dlistint_t));
 	if (newNode == NULL)
 		return (NULL);
 
-	indexCount = 0;
-	newNode = *h;
+	newNode->n = n;
 
-	while (indexCount < idx)
+	if (idx == 0)
 	{
-		newNode = newNode->next;
-		indexCount = indexCount + 1;
+		newNode->prev = NULL;
+		newNode->next = *h;
 
-		if (newNode->next == NULL &&
-		newNode->prev == NULL)
-			return (NULL);
+		if (*h != NULL)
+			(*h)->prev = newNode;
 
+		*h = newNode;
+		return (newNode);
 	}
 
-	newNode = newNode->next;
-	newNode->n = n;
+	current = *h;
+	while (current != NULL && indexCount < idx - 1)
+	{
+		current->next;
+		indexCount = indexCount + 1;
+	}
+
+	if (current == NULL)
+	{
+		free(newNode);
+		return (NULL);
+	}
+
+	newNode->next = current->next;
+	newNode->prev = current;
+
+	if (current->next != NULL)
+		current->next->prev = newNode;
+	current->next = newNode;
 
 	return (newNode);
 }
