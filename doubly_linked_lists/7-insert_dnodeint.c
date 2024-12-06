8c8a06c701596b93f30f5bf3bba57fa40ad240a8
anna <annavyly@gmail.com>
2024-12-06 13:14:33 +0000

    restarting and trying new ways

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,51 +11,42 @@
  * Return: address of new node, or NULL if fails.
  */
 
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h,
-		unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *newNode, *current;
-	int indexCount;
+	unsigned int count = 0;
+	dlistint_t *temp = NULL;
+	dlistint_t *new = NULL;
 
-	if (indexCount == 0)
-
-	newNode = malloc(sizeof(dlistint_t));
-	if (newNode == NULL)
-		return (NULL);
-
-	newNode->n = n;
-
-	if (idx == 0)
+	temp = *h;
+	while (temp != NULL)
 	{
-		newNode->prev = NULL;
-		newNode->next = *h;
+		if (count != idx)
+			temp = temp->next, count++;
+		else
+		{
+			if (count == 0)
+				new = add_dnodeint(h, n);
+			else
+			{
+				new = malloc(sizeof(dlistint_t));
+				if (new == NULL)
+					return (NULL);
+				new->n = n;
 
-		if (*h != NULL)
-			(*h)->prev = newNode;
-
-		*h = newNode;
-		return (newNode);
+				new->next = temp;
+				new->prev = temp->prev;
+				temp->prev = new;
+				temp = new->prev;
+				temp->next = new;
+			}
+			return (new);
+		}
 	}
-
-	current = *h;
-	while (current != NULL && indexCount < idx - 1)
+	if (idx == count)
 	{
-		current->next;
-		indexCount = indexCount + 1;
+		new = add_dnodeint_end(h, n);
+		return (new);
 	}
+	return (NULL);
 
-	if (current == NULL)
-	{
-		free(newNode);
-		return (NULL);
-	}
-
-	newNode->next = current->next;
-	newNode->prev = current;
-
-	if (current->next != NULL)
-		current->next->prev = newNode;
-	current->next = newNode;
-
-	return (newNode);
 }
